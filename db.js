2508650f34eb985ebddcb2c1f3080d647c3f3cdd
Rishit <ui21cs51@iiitsurat.ac.in>
2024-11-23 11:08:41 +0530

    .

--- db.js
+++ db.js
@@ -1,12 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const db = mysql.createConnection(`${process.env.SERVICE_URI}`);
 
 db.connect((err) => {
     if (err) {
